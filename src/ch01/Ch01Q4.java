f8a61907e59ffa26d37db286957d64ffb95ad17b
Qirui Hu <qiruihu@gmail.com>
2018-04-11 17:59:16 -0700

    add files

--- src/ch01/Ch01Q4.java
+++ src/ch01/Ch01Q4.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package ch01;
 
 public class Ch01Q4 {
@@ -20,26 +20,3 @@ public class Ch01Q4 {
 	}
 
 }
-=======
-package ch01;
-
-public class Ch01Q4 {
-
-	public static void main(String[] args) {
-		printSqrt(2);
-
-	}
-
-
-	public static void printSqrt(double x) {
-
-		if (x < 0) {
-			throw new IllegalArgumentException("Argument " + " is smaller than zero");
-		}
-
-		System.out.printf("The square root of %.2f is %.4f", x, Math.sqrt(x));
-
-	}
-
-}
->>>>>>> 729a8515ee0d2a684401ee3be05dc5bce509cb3e
--- src/ch08/Fractal.java
+++ src/ch08/Fractal.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package ch08;
 
 import java.applet.Applet;
@@ -45,52 +45,4 @@ public class Fractal extends Applet {
 		}
 	}
 
-=======
-package ch08;
-
-import java.applet.Applet;
-import java.awt.*; // Provides Graphics, Image
-
-@SuppressWarnings("serial")
-public class Fractal extends Applet {
-	private Image display;
-	private Graphics drawingArea;
-
-
-	public void init() {
-		int height = getSize().height;
-		int width = getSize().width;
-		display = createImage(width, height);
-		drawingArea = display.getGraphics();
-		randomFractal(0, height / 2, width, height / 2, drawingArea);
-	}
-
-
-	public void paint(Graphics g) {
-		g.drawImage(display, 0, 0, null);
-	}
-
-
-	public static void randomFractal(
-			int leftX,
-			int leftY,
-			int rightX,
-			int rightY,
-			Graphics drawingArea) {
-		final int STOP = 4;
-		int midX, midY;
-		int delta;
-		if ((rightX - leftX) <= STOP)
-			drawingArea.drawLine(leftX, leftY, rightX, rightY);
-		else {
-			midX = (leftX + rightX) / 2;
-			midY = (leftY + rightY) / 2;
-			delta = (int) ((Math.random() - 0.5) * (rightX - leftX));
-			midY += delta;
-			randomFractal(leftX, leftY, midX, midY, drawingArea);
-			randomFractal(midX, midY, rightX, rightY, drawingArea);
-		}
-	}
-
->>>>>>> 729a8515ee0d2a684401ee3be05dc5bce509cb3e
 }
